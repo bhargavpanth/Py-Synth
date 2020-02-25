0ae7eca5b17c94ea266a3623a53f38e17cf4ea33
Bhargav Panth <bhargavrpanth@gmail.com>
2020-02-25 19:41:12 +0530

    added method for generating click operations

--- src/episode/episode.py
+++ src/episode/episode.py
@@ -25,4 +25,48 @@ class Episode:
 			raise e
 		else:
 			return episode
+
+	def validate_episode(self):
+		# from the path of the episode
+		# json.load the content
+		# and validate the JSON content
+		pass
+
+
+	def generate_click_operation(self, step):
+		element = step['element']
+
+		if element == 'anchor':
+			if 'text' in step:
+				el = self.selenium_builder.find_element_with_link(step['text'])
+				el.click()
+				time.sleep(1)
+				return el
+		# else:
+		# 	return self.selenium_builder.
+
+		elif element == 'button':
+			if 'text' in step:
+				path = '//button[text()=\'' + step['text'] + '\']'
+				el = self.selenium_builder.find_element()
+				el.click()
+				return el
+			
+			elif 'id' in step:
+				pass
+			
+			elif 'class' in step:
+				pass
+			
+			else:
+				pass
+
+		elif element == 'span':
+			if 'text' in step:
+				path = '//span[text()=\'' + step['text'] + '\']'
+				el = self.selenium_builder.find_element(path)
+				el.click()
+				return el
+
+		# check for ID and class
 		
